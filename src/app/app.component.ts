4998190221b8ef1142b13d6d0c8dfae9e92508fb
NoorAlkhouli <nooralkhouli99@gmail.com>
2024-03-19 15:49:59 +0300

    h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,4 +16,12 @@ export class AppComponent {
   show: boolean = true;
 
   items = ['Noor', 'Samya', 'Rouaa', 'Nabila', 'Ola'];
+
+  name: string = 'My Name';
+
+  isChecked: boolean = true;
+
+  printName() {
+    alert(this.name);
+  }
 }
