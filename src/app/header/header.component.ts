b0039f578104c568e2fd4fa9b40870d904304126
NoorAlkhouli <nooralkhouli99@gmail.com>
2024-03-25 15:51:42 +0300

    hh

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { PostComponent } from "../post/post.component";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink,RouterOutlet],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    imports: [RouterLink, RouterOutlet, PostComponent]
 })
 export class HeaderComponent {
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrl: './post.component.css',
+  imports: [RouterOutlet, HeaderComponent, FormsModule],
 })
 export class PostComponent {
+  login(loginForm:NgForm): void {
 
+    console.log('loginForm', loginForm.valid, loginForm.value);
+  }
 }
