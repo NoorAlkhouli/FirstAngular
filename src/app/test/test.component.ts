ed4f3bad1b3a7cc96fa8ed43dda62036bcee2e63
NoorAlkhouli <nooralkhouli99@gmail.com>
2024-04-01 11:40:12 +0300

    hh

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MultislotComponent } from '../multislot/multislot.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [MultislotComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
